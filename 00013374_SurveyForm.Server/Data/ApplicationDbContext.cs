f5b0e42f470033f614c6a0341034c247b5f4845c
wiut00013374 <Hp@LAPTOP-1N1SQG72>
2024-03-21 15:58:20 +0500

    Configured DbContext

---
+++ 00013374_SurveyForm.Server/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using _00013374_SurveyForm.Server.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace _00013374_SurveyForm.Server.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Question> Questions { get; set; }
+    }
+}
---
+++ 00013374_SurveyForm.Server/Models/Question.cs
@@ -0,0 +1,8 @@
+﻿namespace _00013374_SurveyForm.Server.Models
+{
+    public class Question
+    {
+        public Guid Id { get; set; }
+        public string QuestionText { get; set; }
+    }
+}
